ef08476ac9ab1c2934e5fbff49f28147a518f368
Eddy Gharbi <eddy.gharbi@streamlabs.com>
2019-08-29 13:12:00 -0700

    Rebase fixes

--- obs-browser-source.cpp
+++ obs-browser-source.cpp
@@ -20,12 +20,12 @@
 #include "browser-client.hpp"
 #include "wide-string.hpp"
 #include <util/threading.h>
-#include <QApplication>
 #include <functional>
 #include <thread>
 #include <mutex>
 
 #ifdef USE_QT_LOOP
+#include <QApplication>
 #include <QEventLoop>
 #include <QThread>
 #endif
@@ -483,14 +483,6 @@ void DispatchJSEvent(std::string eventName, std::string jsonString)
 
 		args->SetString(0, eventName);
 		args->SetString(1, jsonString);
-<<<<<<< HEAD
 		SendBrowserProcessMessage(cefBrowser, PID_RENDERER, msg);
-=======
-#if CHROME_VERSION_BUILD >= 3770
-		cefBrowser->GetMainFrame()->SendProcessMessage(PID_RENDERER, msg);
-#else
-		cefBrowser->SendProcessMessage(PID_RENDERER, msg);
-#endif
->>>>>>> Fix change with SendProcessMessage
 	});
 }
